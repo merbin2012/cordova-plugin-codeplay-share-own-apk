dc3d7ff2f12479679e7beb9b590f5491510b461f
Merbin Joe <merbin2010@gmail.com>
2021-11-08 12:59:53 +0530

    Update codeplay_shareapk.js

--- www/codeplay_shareapk.js
+++ www/codeplay_shareapk.js
@@ -2,3 +2,16 @@ var exec = require('cordova/exec');
 exports.openShare = function (arg0,arg1, success, error) {
 	exec(success, error, 'codeplay_shareapk', 'openShare', [arg0,arg1]);
 };
+
+exports.isSupport = function (success, error) {
+	exec(success, error, 'codeplay_shareapk', 'isSupport');
+};
+
+exports.openShareFile = function (arg0,arg1,arg2, success, error) {
+	exec(success, error, 'codeplay_shareapk', 'openShareFile', [arg0,arg1,arg2]);
+};
+
+
+exports.copyFile = function (sourcePath,distinationPath,shareFileName, success, error) {
+	exec(success, error, 'codeplay_shareapk', 'copyFile', [sourcePath,distinationPath,shareFileName]);
+};
